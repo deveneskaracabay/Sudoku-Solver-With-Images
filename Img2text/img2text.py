76e8c64993fcdff47e21fed543a9fca9365ce8b9
Enes Karacabay <deveneskaracabay@gmail.com>
2020-11-13 20:03:16 +0300

    testing now

---
+++ Img2text/img2text.py
@@ -0,0 +1,328 @@
+import pytesseract, cv2, os, copy, shutil
+import numpy as np
+
+class Sudok():
+    def __init__(self,matrix):
+        self.matrix =  matrix
+        if self.control() : 
+            self.flag = self.solve()
+        else:
+            self.flag = False
+
+    def control(self):
+        for row in self.matrix:
+            for i in row:
+                if i<0 or i>9:
+                    return False
+
+        for row in self.matrix:
+            for i in row:
+                if i == 0:
+                    continue
+                if row.count(i) > 1 :
+                    return False
+
+        for i in range(9):
+            temp = []
+            for j in range(9):
+                temp.append(self.matrix[j][i])
+            for i in temp:
+                if i == 0:
+                    continue
+                if temp.count(i)>1:
+                    return False
+                
+        for x0 in [0,3,6]:
+            for y0 in [0,3,6]:
+
+                temp = []
+                for x in range(3):
+                    for y in range(3):
+                        temp.append(self.matrix[x+x0][y+y0])
+
+                for j in temp:
+                    if j == 0:
+                        continue
+                    if temp.count(j)>1:
+                        return False
+
+        return True
+            
+
+    def possible( self, row, col, num):
+        for i in range(9):
+            if self.matrix[row][i] == num:
+                return False
+            
+        for i in range(9):
+            if self.matrix[i][col] == num:
+                return False
+            
+        row0=(row//3)*3
+        col0=(col//3)*3
+        for i in range(3):
+            for j in range(3):
+                if self.matrix[row0+i][col0+j] == num :
+                    return False
+        return True
+
+    def solve(self, row=0, col=0):
+        if (row == 8) and (col == 9):
+            return True
+        
+        if col==9:
+            row += 1
+            col  = 0
+            
+        if (self.matrix[row][col] > 0):
+            return self.solve(row,col+1)
+        
+        for num in range(1,10):
+            if self.possible(row, col, num):
+                self.matrix[row][col] = num
+                if self.solve(row, col+1):
+                    return True
+                self.matrix[row][col] = 0
+
+        return False
+
+def cropIMG(path):
+    try:
+        os.mkdir("temp")
+    except:
+        pass
+
+    dirName = path
+    _,dirName = os.path.split(dirName)
+    dirName = dirName.replace("\\","_"
+    ).replace("/","_"
+    ).strip(".png"
+    ).strip(".jpeg"
+    ).strip(".jpg")
+
+    img_name = dirName
+    
+    dirName = os.path.join("temp",dirName)
+    try:
+        os.mkdir(dirName)
+    except:
+        pass
+
+    #fotoyu okudum
+    img = cv2.imread(path,0)
+
+    #blurladım(köşeler belli olsun diye)
+    img_gray_blur = cv2.GaussianBlur(img,(1,1),cv2.BORDER_DEFAULT)
+
+    ##THRESH BINARY ya beyaz ya da siyah yapıyo)
+    _ , thresh = cv2.threshold(img_gray_blur,150,230,cv2.THRESH_BINARY)
+
+    # Köşeleri alıyorsun
+    contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
+
+    # 81 tane fotoğfar kırpılmasını kontrol ediyorum
+    counter = 10
+
+    # kesilen karelerin koordinatları
+    xy = list()
+
+    # tespit edilen köşeleri geziyoruz
+    for cnt in reversed(contours): 
+
+        # bazen büyük kareleri de dörtgen olarak alıyor
+        if counter == 91:
+            break
+        
+        # köşeler arası uzaklık
+        area = cv2.contourArea(cnt) 
+        
+        # katsayı veriyoruz uzaklık için
+        epsilon = 0.01 * cv2.arcLength(cnt, True)
+
+        ## Her şeklin köşe adresleri
+        approx = cv2.approxPolyDP(cnt,epsilon, True) 
+        
+        if area >400 : 
+
+            # 4 köşegeni olanlar burda ayrılıyor
+            if len(approx)==4: 
+                x1 = approx[0][0][1]
+                y1 = approx[2][0][1]
+                x2 = approx[0][0][0]                
+                y2 = approx[2][0][0]
+
+                # koordinatları kaydediyorum
+                xy.append((x1,y1,x2,y2))
+
+                #x1y1 x2y2 adresleri ile dörtgenleri kırpıyorum
+                rect = img[x1:y1,x2:y2]
+
+                # kesilen bölgeyi okunma ve birleştirme
+                #  işlemi için hazır olmasını sağlıyorum
+                rect = cv2.resize(rect,(100,100))
+                rect = cv2.GaussianBlur(rect,(1,1),cv2.BORDER_DEFAULT)
+                _, rect = cv2.threshold(rect,127,255,cv2.THRESH_BINARY)
+
+                # kırpılan fotograf kaydedilir
+                path = f"{dirName}/{counter}.jpeg"
+                cv2.imwrite(path,rect)
+                counter+=1
+    return dirName, xy ,img_name
+
+def zero2IMG(path):
+
+    # dosyayı geziyorum
+    for p in sorted(os.listdir(path)):
+        
+        # path adresini alıyorum
+        newPath = os.path.join(path,p)
+
+        # fotografı okuyorum
+        img = cv2.imread(newPath,0)
+        
+        # ortalaması 253ten buyukse bos bir fotodur
+        if np.mean(img)>253:
+
+            # sıfırı yazmak için pozisyon alıyorum
+            position = (30,70)
+
+            # yazıyı ekliyorum
+            cv2.putText(
+                img, #fotograf
+                "0", # yazılacak text
+                position, # pozisyon
+                cv2.FONT_HERSHEY_SIMPLEX, #font türü
+                2, #font büyüklüğü
+                (0, 0, 0), #font rengi
+                3) #font gücü
+
+
+            # sıfırlı fotoğrafı kaydediyorum
+            cv2.imwrite(newPath,img)
+
+def concatIMG(path):
+
+    # Bos bir img olusturuyorum
+    img_list = np.zeros((900,900,3))
+    for cnt,pth in enumerate(sorted(os.listdir(path))):
+        # Fotografı okudum
+        newPath = os.path.join(path,pth)
+        img = cv2.imread(newPath)
+        
+        # buyuk fotograftaki nereye kaydedecegimi buluyorum
+        x1 = (cnt // 9) * 100
+        y1 = (cnt % 9)  * 100 
+        x2 = x1 + 100
+        y2 = y1 + 100 
+
+        # Fotoğrafa ekliyorum
+        img_list[x1:x2,y1:y2] = img
+     
+    # Fotografı kaydediyorum
+    pathIMG = 'temp/img.jpeg'
+    cv2.imwrite(pathIMG, img_list)
+
+    # Nereye kaydettiğimi donuyorum
+    return pathIMG
+
+def transformIMG(path):
+
+    # Fotografı okuyorum
+    img = cv2.imread(path)
+
+    # fotografı text haline ceviriyorum
+    txt = pytesseract.image_to_string(img)
+
+    # olusturdugum text degiskenini geri donuyorum
+    return txt
+
+def txt2mat(temp):
+
+    # gelen text dosyasındaki sayıları alıyorum
+    text = ""
+    for i in temp:
+        if i in "0123456789":
+            text = text + i
+
+    # aldıgım sayıları 9 satıra boluyorum
+    temp_list = []
+    for i in range(0,73,9):
+        temp_list.append(text[i:i+9])
+        
+    # tum satirlari int degerlere ceviriyorum 
+    matrix = [[] for j in range(9)]
+    i = 0
+    while i<9:
+        matrix[i][:0] = temp_list[i] 
+        j=0
+        while j<9:
+            matrix[i][j] = int(matrix[i][j])
+            j+=1
+        i += 1
+
+    # Olusturdugum int matrixi donuyorum
+    return matrix
+
+def removeFILES(path):
+    shutil.rmtree(path)
+
+def solution_func(matrix):
+    solution = Sudok(matrix)
+    if solution.flag:
+        return solution.matrix
+    else:
+        print("hatalı işlem")
+
+def writeAnswer2IMG(imgname,path,xy,mat1,mat2):
+    try:
+        os.mkdir("Solution")
+    except:
+        pass
+    
+    img = cv2.imread(path)
+    cnt = 0
+    j,cnt = 0,0
+
+    while j<9:
+        i = 0
+        while i < 9:
+
+            # eğer eski eleman değil ise
+            # yeni değerleri yazıyorum
+            if mat1[i][j]!=mat2[i][j]:
+
+                # yazılacak rakamı alıyorum
+                text = mat2[i][j]
+
+                # yazılacak pozisyonu belirliyorum
+                x = (xy[cnt][0] + xy[cnt][1]) // 2
+                y = (xy[cnt][2] + xy[cnt][3]) // 2
+                position = (x-10,y+15)
+
+                # yazıyı ekliyorum
+                cv2.putText(
+                    img, #fotograf
+                    str(text), # yazılacak text
+                    position, # pozisyon
+                    cv2.FONT_HERSHEY_SIMPLEX, #font türü
+                    1.4, #font büyüklüğü
+                    (0, 255, 0), #font rengi
+                    3) #font gücü    
+
+            cnt+=1
+            i+=1
+        j+=1
+
+    cv2.imwrite(f"Solution/solution{imgname}.jpeg",img)
+
+def solver(path):
+
+    dirName, xy, img_name = cropIMG(path)
+    zero2IMG(dirName)
+    pathIMG = concatIMG(dirName)
+    temp = transformIMG(pathIMG)
+    matrix = txt2mat(temp)
+    oldMatrix = copy.deepcopy(matrix)
+    removeFILES("temp")
+    solutionMatrix = solution_func(matrix)
+    writeAnswer2IMG(img_name,path,xy,oldMatrix,solutionMatrix)
---
+++ sudokuSolver.py
@@ -0,0 +1,541 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+from qt5_file.ui import Ui_MainWindow
+from decoder.sudokuClass import Sudoku
+from Img2text import img2text
+from pathlib import Path
+import sys
+
+def error():    
+    QMessageBox.about(Qmain,"Error","Please enter valid numbers")
+
+def getitem():
+    matrix = [[0 for i in range(9)] for j in range(9)]
+    try:
+        matrix[0][0] = int(ui.lineEdit_1.text())
+    except:
+        matrix[0][0]=0
+    try:
+        matrix[0][1] = int(ui.lineEdit_2.text())
+    except:
+        matrix[0][1]=0
+    try:
+        matrix[0][2] = int(ui.lineEdit_3.text())
+    except:
+        matrix[0][2]=0
+    try:
+        matrix[0][3] = int(ui.lineEdit_4.text())
+    except:
+        matrix[0][3]=0
+    try:
+        matrix[0][4] = int(ui.lineEdit_5.text())
+    except:
+        matrix[0][4]=0
+    try:
+        matrix[0][5] = int(ui.lineEdit_6.text())
+    except:
+        matrix[0][5]=0
+    try:
+        matrix[0][6] = int(ui.lineEdit_7.text())
+    except:
+        matrix[0][6]=0
+    try:
+        matrix[0][7] = int(ui.lineEdit_8.text())
+    except:
+        matrix[0][7]=0
+    try:
+        matrix[0][8] = int(ui.lineEdit_9.text())
+    except:
+        matrix[0][8]=0
+    try:
+        matrix[1][0] = int(ui.lineEdit_10.text())
+    except:
+        matrix[1][0]=0
+    try:
+        matrix[1][1] = int(ui.lineEdit_11.text())
+    except:
+        matrix[1][1]=0
+    try:
+        matrix[1][2] = int(ui.lineEdit_12.text())
+    except:
+        matrix[1][2]=0
+    try:
+        matrix[1][3] = int(ui.lineEdit_13.text())
+    except:
+        matrix[1][3]=0
+    try:
+        matrix[1][4] = int(ui.lineEdit_14.text())
+    except:
+        matrix[1][4]=0
+    try:
+        matrix[1][5] = int(ui.lineEdit_15.text())
+    except:
+        matrix[1][5]=0
+    try:
+        matrix[1][6] = int(ui.lineEdit_16.text())
+    except:
+        matrix[1][6]=0
+    try:
+        matrix[1][7] = int(ui.lineEdit_17.text())
+    except:
+        matrix[1][7]=0
+    try:
+        matrix[1][8] = int(ui.lineEdit_18.text())
+    except:
+        matrix[1][8]=0
+    try:
+        matrix[2][0] = int(ui.lineEdit_19.text())
+    except:
+        matrix[2][0]=0
+    try:
+        matrix[2][1] = int(ui.lineEdit_20.text())
+    except:
+        matrix[2][1]=0
+    try:
+        matrix[2][2] = int(ui.lineEdit_21.text())
+    except:
+        matrix[2][2]=0
+    try:
+        matrix[2][3] = int(ui.lineEdit_22.text())
+    except:
+        matrix[2][3]=0
+    try:
+        matrix[2][4] = int(ui.lineEdit_23.text())
+    except:
+        matrix[2][4]=0
+    try:
+        matrix[2][5] = int(ui.lineEdit_24.text())
+    except:
+        matrix[2][5]=0
+    try:
+        matrix[2][6] = int(ui.lineEdit_25.text())
+    except:
+        matrix[2][6]=0
+    try:
+        matrix[2][7] = int(ui.lineEdit_26.text())
+    except:
+        matrix[2][7]=0
+    try:
+        matrix[2][8] = int(ui.lineEdit_27.text())
+    except:
+        matrix[2][8]=0
+    try:
+        matrix[3][0] = int(ui.lineEdit_28.text())
+    except:
+        matrix[3][0]=0
+    try:
+        matrix[3][1] = int(ui.lineEdit_29.text())
+    except:
+        matrix[3][1]=0
+    try:
+        matrix[3][2] = int(ui.lineEdit_30.text())
+    except:
+        matrix[3][2]=0
+    try:
+        matrix[3][3] = int(ui.lineEdit_31.text())
+    except:
+        matrix[3][3]=0
+    try:
+        matrix[3][4] = int(ui.lineEdit_32.text())
+    except:
+        matrix[3][4]=0
+    try:
+        matrix[3][5] = int(ui.lineEdit_33.text())
+    except:
+        matrix[3][5]=0
+    try:
+        matrix[3][6] = int(ui.lineEdit_34.text())
+    except:
+        matrix[3][6]=0
+    try:
+        matrix[3][7] = int(ui.lineEdit_35.text())
+    except:
+        matrix[3][7]=0
+    try:
+        matrix[3][8] = int(ui.lineEdit_36.text())
+    except:
+        matrix[3][8]=0
+    try:
+        matrix[4][0] = int(ui.lineEdit_37.text())
+    except:
+        matrix[4][0]=0
+    try:
+        matrix[4][1] = int(ui.lineEdit_38.text())
+    except:
+        matrix[4][1]=0
+    try:
+        matrix[4][2] = int(ui.lineEdit_39.text())
+    except:
+        matrix[4][2]=0
+    try:
+        matrix[4][3] = int(ui.lineEdit_40.text())
+    except:
+        matrix[4][3]=0
+    try:
+        matrix[4][4] = int(ui.lineEdit_41.text())
+    except:
+        matrix[4][4]=0
+    try:
+        matrix[4][5] = int(ui.lineEdit_42.text())
+    except:
+        matrix[4][5]=0
+    try:
+        matrix[4][6] = int(ui.lineEdit_43.text())
+    except:
+        matrix[4][6]=0
+    try:
+        matrix[4][7] = int(ui.lineEdit_44.text())
+    except:
+        matrix[4][7]=0
+    try:
+        matrix[4][8] = int(ui.lineEdit_45.text())
+    except:
+        matrix[4][8]=0
+    try:
+        matrix[5][0] = int(ui.lineEdit_46.text())
+    except:
+        matrix[5][0]=0
+    try:
+        matrix[5][1] = int(ui.lineEdit_47.text())
+    except:
+        matrix[5][1]=0
+    try:
+        matrix[5][2] = int(ui.lineEdit_48.text())
+    except:
+        matrix[5][2]=0
+    try:
+        matrix[5][3] = int(ui.lineEdit_49.text())
+    except:
+        matrix[5][3]=0
+    try:
+        matrix[5][4] = int(ui.lineEdit_50.text())
+    except:
+        matrix[5][4]=0
+    try:
+        matrix[5][5] = int(ui.lineEdit_51.text())
+    except:
+        matrix[5][5]=0
+    try:
+        matrix[5][6] = int(ui.lineEdit_52.text())
+    except:
+        matrix[5][6]=0
+    try:
+        matrix[5][7] = int(ui.lineEdit_53.text())
+    except:
+        matrix[5][7]=0
+    try:
+        matrix[5][8] = int(ui.lineEdit_54.text())
+    except:
+        matrix[5][8]=0
+
+    try:
+        matrix[6][0] = int(ui.lineEdit_55.text())
+    except:
+        matrix[6][0]=0
+    try:
+        matrix[6][1] = int(ui.lineEdit_56.text())
+    except:
+        matrix[6][1]=0
+    try:
+        matrix[6][2] = int(ui.lineEdit_57.text())
+    except:
+        matrix[6][2]=0
+    try:
+        matrix[6][3] = int(ui.lineEdit_58.text())
+    except:
+        matrix[6][3]=0
+    try:
+        matrix[6][4] = int(ui.lineEdit_59.text())
+    except:
+        matrix[6][4]=0
+    try:
+        matrix[6][5] = int(ui.lineEdit_60.text())
+    except:
+        matrix[6][5]=0
+    try:
+        matrix[6][6] = int(ui.lineEdit_61.text())
+    except:
+        matrix[6][6]=0
+    try:
+        matrix[6][7] = int(ui.lineEdit_62.text())
+    except:
+        matrix[6][7]=0
+    try:
+        matrix[6][8] = int(ui.lineEdit_63.text())
+    except:
+        matrix[6][8]=0
+    try:
+        matrix[7][0] = int(ui.lineEdit_64.text())
+    except:
+        matrix[7][0]=0
+    try:
+        matrix[7][1] = int(ui.lineEdit_65.text())
+    except:
+        matrix[7][1]=0
+    try:
+        matrix[7][2] = int(ui.lineEdit_66.text())
+    except:
+        matrix[7][2]=0
+    try:
+        matrix[7][3] = int(ui.lineEdit_67.text())
+    except:
+        matrix[7][3]=0
+    try:
+        matrix[7][4] = int(ui.lineEdit_68.text())
+    except:
+        matrix[7][4]=0
+    try:
+        matrix[7][5] = int(ui.lineEdit_69.text())
+    except:
+        matrix[7][5]=0
+    try:
+        matrix[7][6] = int(ui.lineEdit_70.text())
+    except:
+        matrix[7][6]=0
+    try:
+        matrix[7][7] = int(ui.lineEdit_71.text())
+    except:
+        matrix[7][7]=0
+    try:
+        matrix[7][8] = int(ui.lineEdit_72.text())
+    except:
+        matrix[7][8]=0
+    try:
+        matrix[8][0] = int(ui.lineEdit_73.text())
+    except:
+        matrix[8][0]=0
+    try:
+        matrix[8][1] = int(ui.lineEdit_74.text())
+    except:
+        matrix[8][1]=0
+    try:
+        matrix[8][2] = int(ui.lineEdit_75.text())
+    except:
+        matrix[8][2]=0
+    try:
+        matrix[8][3] = int(ui.lineEdit_76.text())
+    except:
+        matrix[8][3]=0
+    try:
+        matrix[8][4] = int(ui.lineEdit_77.text())
+    except:
+        matrix[8][4]=0
+    try:
+        matrix[8][5] = int(ui.lineEdit_78.text())
+    except:
+        matrix[8][5]=0
+    try:
+        matrix[8][6] = int(ui.lineEdit_79.text())
+    except:
+        matrix[8][6]=0
+    try:
+        matrix[8][7] = int(ui.lineEdit_80.text())
+    except:
+        matrix[8][7]=0
+    try:
+        matrix[8][8] = int(ui.lineEdit_81.text())
+    except:
+        matrix[8][8]=0
+
+    solution_func(matrix)
+     
+def reset():
+    ui.lineEdit_1.setText ("")
+    ui.lineEdit_2.setText ("")
+    ui.lineEdit_3.setText ("")
+    ui.lineEdit_4.setText ("")
+    ui.lineEdit_5.setText ("")
+    ui.lineEdit_6.setText ("")
+    ui.lineEdit_7.setText ("")
+    ui.lineEdit_8.setText ("")
+    ui.lineEdit_9.setText ("")
+    ui.lineEdit_10.setText("")
+    ui.lineEdit_11.setText("")
+    ui.lineEdit_12.setText("")
+    ui.lineEdit_13.setText("")
+    ui.lineEdit_14.setText("")
+    ui.lineEdit_15.setText("")
+    ui.lineEdit_16.setText("")
+    ui.lineEdit_17.setText("")
+    ui.lineEdit_18.setText("")
+    ui.lineEdit_19.setText("")
+    ui.lineEdit_20.setText("")
+    ui.lineEdit_21.setText("")
+    ui.lineEdit_22.setText("")
+    ui.lineEdit_23.setText("")
+    ui.lineEdit_24.setText("")
+    ui.lineEdit_25.setText("")
+    ui.lineEdit_26.setText("")
+    ui.lineEdit_27.setText("")
+    ui.lineEdit_28.setText("")
+    ui.lineEdit_29.setText("")
+    ui.lineEdit_30.setText("")
+    ui.lineEdit_31.setText("")
+    ui.lineEdit_32.setText("")
+    ui.lineEdit_33.setText("")
+    ui.lineEdit_34.setText("")
+    ui.lineEdit_35.setText("")
+    ui.lineEdit_36.setText("")
+    ui.lineEdit_37.setText("")
+    ui.lineEdit_38.setText("")
+    ui.lineEdit_39.setText("")
+    ui.lineEdit_40.setText("")
+    ui.lineEdit_41.setText("")
+    ui.lineEdit_42.setText("")
+    ui.lineEdit_43.setText("")
+    ui.lineEdit_44.setText("")
+    ui.lineEdit_45.setText("")
+    ui.lineEdit_46.setText("")
+    ui.lineEdit_47.setText("")
+    ui.lineEdit_48.setText("")
+    ui.lineEdit_49.setText("")
+    ui.lineEdit_50.setText("")
+    ui.lineEdit_51.setText("")
+    ui.lineEdit_52.setText("")
+    ui.lineEdit_53.setText("")
+    ui.lineEdit_54.setText("")
+    ui.lineEdit_55.setText("")
+    ui.lineEdit_56.setText("")
+    ui.lineEdit_57.setText("")
+    ui.lineEdit_58.setText("")
+    ui.lineEdit_59.setText("")
+    ui.lineEdit_60.setText("")
+    ui.lineEdit_61.setText("")
+    ui.lineEdit_62.setText("")
+    ui.lineEdit_63.setText("")
+    ui.lineEdit_64.setText("")
+    ui.lineEdit_65.setText("")
+    ui.lineEdit_66.setText("")
+    ui.lineEdit_67.setText("")
+    ui.lineEdit_68.setText("")
+    ui.lineEdit_69.setText("")
+    ui.lineEdit_70.setText("")
+    ui.lineEdit_71.setText("")
+    ui.lineEdit_72.setText("")
+    ui.lineEdit_73.setText("")
+    ui.lineEdit_74.setText("")
+    ui.lineEdit_75.setText("")
+    ui.lineEdit_76.setText("")
+    ui.lineEdit_77.setText("")
+    ui.lineEdit_78.setText("")
+    ui.lineEdit_79.setText("")
+    ui.lineEdit_80.setText("")
+    ui.lineEdit_81.setText("")
+
+def openFile():
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    fileName, _ = QFileDialog.getOpenFileName(Qmain, 
+        "QFileDialog.getOpenFileName()", 
+        options=options)
+    if fileName:
+        img2text.solver(fileName)
+        return
+    else:
+        return
+
+def additem(solution):
+    ui.lineEdit_1.setText (str(solution.matrix[0][0]))
+    ui.lineEdit_2.setText (str(solution.matrix[0][1]))
+    ui.lineEdit_3.setText (str(solution.matrix[0][2]))
+    ui.lineEdit_4.setText (str(solution.matrix[0][3]))
+    ui.lineEdit_5.setText (str(solution.matrix[0][4]))
+    ui.lineEdit_6.setText (str(solution.matrix[0][5]))
+    ui.lineEdit_7.setText (str(solution.matrix[0][6]))
+    ui.lineEdit_8.setText (str(solution.matrix[0][7]))
+    ui.lineEdit_9.setText (str(solution.matrix[0][8]))
+    ui.lineEdit_10.setText(str(solution.matrix[1][0]))
+    ui.lineEdit_11.setText(str(solution.matrix[1][1]))
+    ui.lineEdit_12.setText(str(solution.matrix[1][2]))
+    ui.lineEdit_13.setText(str(solution.matrix[1][3]))
+    ui.lineEdit_14.setText(str(solution.matrix[1][4]))
+    ui.lineEdit_15.setText(str(solution.matrix[1][5]))
+    ui.lineEdit_16.setText(str(solution.matrix[1][6]))
+    ui.lineEdit_17.setText(str(solution.matrix[1][7]))
+    ui.lineEdit_18.setText(str(solution.matrix[1][8]))
+    ui.lineEdit_19.setText(str(solution.matrix[2][0]))
+    ui.lineEdit_20.setText(str(solution.matrix[2][1]))
+    ui.lineEdit_21.setText(str(solution.matrix[2][2]))
+    ui.lineEdit_22.setText(str(solution.matrix[2][3]))
+    ui.lineEdit_23.setText(str(solution.matrix[2][4]))
+    ui.lineEdit_24.setText(str(solution.matrix[2][5]))
+    ui.lineEdit_25.setText(str(solution.matrix[2][6]))
+    ui.lineEdit_26.setText(str(solution.matrix[2][7]))
+    ui.lineEdit_27.setText(str(solution.matrix[2][8]))
+    ui.lineEdit_28.setText(str(solution.matrix[3][0]))
+    ui.lineEdit_29.setText(str(solution.matrix[3][1]))
+    ui.lineEdit_30.setText(str(solution.matrix[3][2]))
+    ui.lineEdit_31.setText(str(solution.matrix[3][3]))
+    ui.lineEdit_32.setText(str(solution.matrix[3][4]))
+    ui.lineEdit_33.setText(str(solution.matrix[3][5]))
+    ui.lineEdit_34.setText(str(solution.matrix[3][6]))
+    ui.lineEdit_35.setText(str(solution.matrix[3][7]))
+    ui.lineEdit_36.setText(str(solution.matrix[3][8]))
+    ui.lineEdit_37.setText(str(solution.matrix[4][0]))
+    ui.lineEdit_38.setText(str(solution.matrix[4][1]))
+    ui.lineEdit_39.setText(str(solution.matrix[4][2]))
+    ui.lineEdit_40.setText(str(solution.matrix[4][3]))
+    ui.lineEdit_41.setText(str(solution.matrix[4][4]))
+    ui.lineEdit_42.setText(str(solution.matrix[4][5]))
+    ui.lineEdit_43.setText(str(solution.matrix[4][6]))
+    ui.lineEdit_44.setText(str(solution.matrix[4][7]))
+    ui.lineEdit_45.setText(str(solution.matrix[4][8]))
+    ui.lineEdit_46.setText(str(solution.matrix[5][0]))
+    ui.lineEdit_47.setText(str(solution.matrix[5][1]))
+    ui.lineEdit_48.setText(str(solution.matrix[5][2]))
+    ui.lineEdit_49.setText(str(solution.matrix[5][3]))
+    ui.lineEdit_50.setText(str(solution.matrix[5][4]))
+    ui.lineEdit_51.setText(str(solution.matrix[5][5]))
+    ui.lineEdit_52.setText(str(solution.matrix[5][6]))
+    ui.lineEdit_53.setText(str(solution.matrix[5][7]))
+    ui.lineEdit_54.setText(str(solution.matrix[5][8]))
+    ui.lineEdit_55.setText(str(solution.matrix[6][0]))
+    ui.lineEdit_56.setText(str(solution.matrix[6][1]))
+    ui.lineEdit_57.setText(str(solution.matrix[6][2]))
+    ui.lineEdit_58.setText(str(solution.matrix[6][3]))
+    ui.lineEdit_59.setText(str(solution.matrix[6][4]))
+    ui.lineEdit_60.setText(str(solution.matrix[6][5]))
+    ui.lineEdit_61.setText(str(solution.matrix[6][6]))
+    ui.lineEdit_62.setText(str(solution.matrix[6][7]))
+    ui.lineEdit_63.setText(str(solution.matrix[6][8]))
+    ui.lineEdit_64.setText(str(solution.matrix[7][0]))
+    ui.lineEdit_65.setText(str(solution.matrix[7][1]))
+    ui.lineEdit_66.setText(str(solution.matrix[7][2]))
+    ui.lineEdit_67.setText(str(solution.matrix[7][3]))
+    ui.lineEdit_68.setText(str(solution.matrix[7][4]))
+    ui.lineEdit_69.setText(str(solution.matrix[7][5]))
+    ui.lineEdit_70.setText(str(solution.matrix[7][6]))
+    ui.lineEdit_71.setText(str(solution.matrix[7][7]))
+    ui.lineEdit_72.setText(str(solution.matrix[7][8]))
+    ui.lineEdit_73.setText(str(solution.matrix[8][0]))
+    ui.lineEdit_74.setText(str(solution.matrix[8][1]))
+    ui.lineEdit_75.setText(str(solution.matrix[8][2]))
+    ui.lineEdit_76.setText(str(solution.matrix[8][3]))
+    ui.lineEdit_77.setText(str(solution.matrix[8][4]))
+    ui.lineEdit_78.setText(str(solution.matrix[8][5]))
+    ui.lineEdit_79.setText(str(solution.matrix[8][6]))
+    ui.lineEdit_80.setText(str(solution.matrix[8][7]))
+    ui.lineEdit_81.setText(str(solution.matrix[8][8]))
+
+def solution_func(matrix):
+    solution = Sudoku(matrix)
+    if solution.flag:
+        return additem(solution)
+    else:
+        error()
+   
+
+
+app = QApplication(sys.argv)
+Qmain = QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(Qmain)
+ui.Button.clicked.connect(getitem)
+ui.Button_2.clicked.connect(reset)
+ui.Button_3.clicked.connect(openFile)
+
+Qmain.show()
+sys.exit(app.exec_())
+
+
+
+
